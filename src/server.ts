9ba676f2406c10d2d1fc2c0ebbe30086e03bfd67
nerokome <nerooghenekome@gmail.com>
2026-01-10 10:01:30 +0100

    fixed cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 import express from "express";
 import { createServer } from "http";
 import "dotenv/config";
+import cors from "cors"; // <-- import cors
 
 import authRoutes from "./routes/auth.routes";
 import { initSocket } from "./socket";
 
 const app = express();
+
+
+app.use(cors({ 
+  origin:"http://localhost:3000"
+}));
 app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
 
 app.get("/", (_req, res) => res.send("Server running!"));
+
+
 const httpServer = createServer(app);
 initSocket(httpServer);
+
 
 const PORT = Number(process.env.PORT) || 5000;
 httpServer.listen(PORT, () => {
